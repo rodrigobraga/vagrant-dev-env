2148de73537c0d2420ec235996595be1b1f18d55
Rodrigo Braga <rodrigobraga@msn.com>
2014-04-26 16:01:07 -0300

    some updates

--- shell-scripts/heroku.sh
+++ shell-scripts/heroku.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+echo "
+
+# heroku.com CONFIG
+  Host heroku.com
+  HostName heroku.com
+  PreferredAuthentications publickey
+  IdentityFile /home/vagrant/.ssh/bitbucket.org_id_rsa" >> ~/.ssh/config
