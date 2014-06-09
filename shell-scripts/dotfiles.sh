64d75faeec7c213ab2a81d791c98a0fe2c458a96
Rodrigo Braga <rodrigobraga@msn.com>
2014-06-09 08:35:49 -0300

    copying to "home" from Vagrant

--- shell-scripts/dotfiles.sh
+++ shell-scripts/dotfiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp /vagrant/dotfiles/.* ~/
+cp /vagrant/dotfiles/.* /home/vagrant
 
 for f in ".bash_profile" ".bashrc" ".gitconfig" ".jshintrc" ".jslintrc" ".screenrc" ".vimrc"
 do
-    dos2unix ~/$f
+    dos2unix /home/vagrant/$f
 done
 
 source ~/.bash_profile
