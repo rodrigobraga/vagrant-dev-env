e1b4de8573a9a3a1091c98ddab25a78c5933547c
Rodrigo Braga <rodrigobraga@msn.com>
2014-09-28 08:25:22 -0300

    remove node tools

--- shell-scripts/node.sh
+++ shell-scripts/node.sh
@@ -6,12 +6,3 @@ apt-get install -y nodejs
 
 # set node path
 echo 'if [ -d "/usr/lib/node_modules" ]; then NODE_PATH="/usr/lib/node_modules"; fi' >> ~/.profile
-
-# task tools
-npm install -g grunt
-
-# package manager
-npm install -g bower
-
-# RequireJS / r.js
-npm install -g requirejs
