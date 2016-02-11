b0059f448fefe4d3528cdf6e8dab5bf4a100ae98
Rodrigo Braga <rodrigobraga@msn.com>
2016-02-11 21:20:03 -0200

    Add Docker Machine

--- shell-scripts/docker.sh
+++ shell-scripts/docker.sh
@@ -7,9 +7,14 @@ usermod -aG docker vagrant
 
 # compose
 curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > docker-compose
-mv docker-compose /usr/local/bin/docker-compose
+mv docker-compose /usr/local/bin/
 chmod +x /usr/local/bin/docker-compose
 
 # compose command completion
 curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose --version | awk 'NR==1{print $NF}')/contrib/completion/bash/docker-compose > docker-compose-completion
 mv docker-compose-completion /etc/bash_completion.d/docker-compose
+
+# machine
+curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > docker-machine
+mv docker-machine /usr/local/bin/
+chmod +x /usr/local/bin/docker-machine
