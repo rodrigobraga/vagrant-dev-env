2ddb0aae82700e9cf09e1a7217e9dd2be59f83e9
Rodrigo Braga <rodrigobraga@msn.com>
2016-11-26 10:24:11 -0200

    update compose

--- shell-scripts/docker.sh
+++ shell-scripts/docker.sh
@@ -6,7 +6,7 @@ curl -sSL https://get.docker.com/ | sh
 usermod -aG docker ubuntu
 
 # compose
-curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > docker-compose
 mv docker-compose /usr/local/bin/
 chmod +x /usr/local/bin/docker-compose
 
